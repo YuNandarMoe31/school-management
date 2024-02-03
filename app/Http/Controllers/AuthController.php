0f32ce0333116bd7bc5fd306af9f5d4e8d012014
YuNandarMoe <yunandarmoe31@gmail.com>
2024-02-03 18:48:53 +0630

    role management (admin, user middleware)

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -10,20 +10,21 @@ class AuthController extends Controller
 {
     public function login()
     {
+        if (!empty(Auth::check())) {
+            if (Auth::user()->user_type == 1) {
+                return redirect()->intended('admin/dashboard');
+            } else if (Auth::user()->user_type == 2) {
+                return redirect()->intended('teacher/dashboard');
+            } else if (Auth::user()->user_type == 3) {
+                return redirect()->intended('student/dashboard');
+            } else if (Auth::user()->user_type == 4)
+                return redirect()->intended('parent/dashboard');
+        }
         return view('auth.login');
     }
 
     public function authLogin(Request $request)
     {
-        // $remember = !empty($request->remember) ? true : false;
-        // if (Auth::attempt([
-        //     'email' => $request->email,
-        //     'password' => $request->password
-        // ], true)) {
-        //     return redirect('admin/dashboard');
-        // } else {
-        //     return redirect()->back()->with('error', 'Please enter correct email and password');
-        // };
         // Validate the form data
         $credentials = $request->validate([
             'email' => 'required|email',
@@ -34,7 +35,14 @@ class AuthController extends Controller
         $remember = $request->has('remember'); // Check if remember me is checked
         if (Auth::attempt($credentials, $remember)) {
             // Authentication passed
-            return redirect()->intended('admin/dashboard');
+            if (Auth::user()->user_type == 1) {
+                return redirect()->intended('admin/dashboard');
+            } else if (Auth::user()->user_type == 2) {
+                return redirect()->intended('teacher/dashboard');
+            } else if (Auth::user()->user_type == 3) {
+                return redirect()->intended('student/dashboard');
+            } else if (Auth::user()->user_type == 4)
+                return redirect()->intended('parent/dashboard');
         } else {
             // Authentication failed
             return redirect()->back()->with('error', 'Please enter correct email and password');
--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,31 @@ Route::get('/', [AuthController::class, 'login'])->name('login');
 Route::post('/', [AuthController::class, 'authLogin']);
 Route::get('logout', [AuthController::class, 'logout']);
 
-Route::get('admin/dashboard', function () {
-    return view('admin.dashboard');
-});
 
 Route::get('admin/admin/list', function () {
     return view('admin.list');
+});
+
+Route::group(['middleware' => 'admin'], function () {
+    Route::get('admin/dashboard', function () {
+        return view('admin.dashboard');
+    });
+});
+
+Route::group(['middleware' => 'teacher'], function () {
+    Route::get('teacher/dashboard', function () {
+        return view('teacher.dashboard');
+    });
+});
+
+Route::group(['middleware' => 'student'], function () {
+    Route::get('student/dashboard', function () {
+        return view('student.dashboard');
+    });
+});
+
+Route::group(['middleware' => 'parent'], function () {
+    Route::get('parent/dashboard', function () {
+        return view('parent.dashboard');
+    });
 });
